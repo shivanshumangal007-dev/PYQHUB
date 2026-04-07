f84d1e974b826ca86b49b08bd3683c96c1e52076
Shivanshu Mangal <shivanshumangal007@gmail.com>
2026-04-07 16:02:12 +0530

    addded gitignore

---
+++ app/auth.py
@@ -0,0 +1,28 @@
+from jose import jwt
+from datetime import datetime, timedelta
+
+SECRET_KEY = "your_super_secret_key"
+ALGORITHM = "HS256"
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+REFRESH_TOKEN_EXPIRE_DAYS = 7
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    to_encode.update({"exp": expire})
+    
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def create_refresh_token(data: dict):
+    to_encode = data.copy()
+    
+    expire = datetime.utcnow() + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    
+    to_encode.update({"exp": expire})
+    
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
